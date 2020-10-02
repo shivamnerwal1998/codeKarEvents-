c174b747629b8efbd91d5918524a8bd73a85f2d2
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2020-10-02 14:06:01 +0530

    Tony's friend
    pdf file contains the probelm statement and cpp file contain the solution of problem

---
+++ tonny.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int testCases ;
+    cin >> testCases ; // total test sets
+    int totalFriends ;
+    int minSat ;
+    int i;
+    while( testCases > 0)
+    {
+        cin >> totalFriends ; // total friends
+        testCases-- ;
+        int sat[totalFriends] ;
+        for( i = 0 ; i <= (totalFriends-1) ; i++ )
+            cin >> sat[i] ;
+        cin >> minSat;  // minimum friends to be satisified
+        sort(sat,sat+totalFriends); // sorting the no of gifts for each satisifaction
+        int minimumGifts = 0 ;
+        for(i=0 ; i<= minSat-1 ; i++)
+            minimumGifts += sat[i] ;
+        cout<< minimumGifts ;
+
+
+
+
+    }
+
+    return 0;
+}
